8b7c5d7744c04f947afe981ae4830fe3e178d4a9
irfan.hassan <irfan.hassan@gmail.com>
2026-06-02 22:13:01 +1000

    fix pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ cd "${BUILDKITE_BUILD_CHECKOUT_PATH}/.buildkite"
 npm ci
 
 echo "--- Compiling TypeScript"
-node --max-old-space-size=512 node_modules/.bin/tsc
+node --max-old-space-size=1024 node_modules/.bin/tsc --skipLibCheck
 
 echo "--- Deploying to Fargate"
 npx cdk deploy --require-approval never --parameters "ImageTag=${BUILDKITE_COMMIT}" --region ap-southeast-2
