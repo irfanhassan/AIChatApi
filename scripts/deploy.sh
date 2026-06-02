ccfc88fda1eded54078094f198abe4ae599e82d0
irfan.hassan <irfan.hassan@gmail.com>
2026-06-02 22:02:07 +1000

    fix pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,8 @@ echo "--- Installing CDK dependencies"
 cd "${BUILDKITE_BUILD_CHECKOUT_PATH}/.buildkite"
 npm ci
 
+echo "--- Compiling TypeScript"
+npx tsc
+
 echo "--- Deploying to Fargate"
-npx cdk deploy --require-approval never --parameters "ImageTag=${BUILDKITE_COMMIT}" --region ap-southeast-2 --verbose
+npx cdk deploy --require-approval never --parameters "ImageTag=${BUILDKITE_COMMIT}" --region ap-southeast-2
