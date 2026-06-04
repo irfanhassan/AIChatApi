5a0f5d9c07ca1a87e9562e07b50ff1b8c16d168d
irfan.hassan <irfan.hassan@gmail.com>
2026-06-04 11:15:55 +1000

    switch to OpenAIClient.GetChatClient() to fix model not sent in requests

--- Services/OpenAiClient.cs
+++ Services/OpenAiClient.cs
@@ -15,18 +15,19 @@ public class OpenAiClient(ChatClient chatClient) : IAiClient
             })
             .ToList();
 
+        var hasOptions = options is { Temperature: not null } or { TopP: not null } or { MaxTokens: not null };
         ChatCompletionOptions? chatOptions = null;
-        if (options is { Temperature: not null } or { TopP: not null } or { MaxTokens: not null })
+        if (hasOptions)
         {
             chatOptions = new ChatCompletionOptions();
-            if (options.Temperature is not null) chatOptions.Temperature       = options.Temperature;
-            if (options.TopP is not null)         chatOptions.TopP              = options.TopP;
-            if (options.MaxTokens is not null)    chatOptions.MaxOutputTokenCount = options.MaxTokens;
+            if (options!.Temperature is not null) chatOptions.Temperature        = options.Temperature;
+            if (options.TopP is not null)          chatOptions.TopP               = options.TopP;
+            if (options.MaxTokens is not null)     chatOptions.MaxOutputTokenCount = options.MaxTokens;
         }
 
-        var completion = chatOptions is null
-            ? await chatClient.CompleteChatAsync(openAiMessages)
-            : await chatClient.CompleteChatAsync(openAiMessages, chatOptions);
+        var completion = hasOptions
+            ? await chatClient.CompleteChatAsync(openAiMessages, chatOptions)
+            : await chatClient.CompleteChatAsync(openAiMessages);
         return completion.Value.Content[0].Text;
     }
 }
